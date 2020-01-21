ea55a1ef967f5539410da1a07de0acec16d18445
Phan Trung Kien <trungkien.phan1991@gmail.com>
2020-01-21 14:40:41 +0700

    Write some specs

--- spec/requests/api/v1/hotels_spec.rb
+++ spec/requests/api/v1/hotels_spec.rb
@@ -7,9 +7,39 @@ RSpec.describe 'Api::V1::Hotels', type: :request do
     Hotel.crawling_data
   end
   describe 'GET /v1/hotels.json' do
-    it 'Show list of hotels with parameters' do
+    it 'Show list of hotels without parameters' do
       get v1_hotels_path(format: 'json')
       expect(response).to have_http_status(200)
+      hotels = JSON.parse(response.body)
+      expect(hotels.count).to eq(3)
+    end
+
+    it 'works with param hotels' do
+      get v1_hotels_path(format: 'json', hotels: 'f8c9,SjyX')
+      expect(response).to have_http_status(200)
+      hotels = JSON.parse(response.body)
+      hotel_ids = hotels.map { |h| h['id'] }
+      expect(hotels.count).to eq(2)
+      expect(hotel_ids).to include('f8c9', 'SjyX')
+    end
+
+    it 'works with both of params hotels and destination_id' do
+      get v1_hotels_path(format: 'json', hotels: 'f8c9,SjyX', destination_id: 5432)
+      expect(response).to have_http_status(200)
+      hotels = JSON.parse(response.body)
+      hotel_ids = hotels.map { |h| h['id'] }
+      expect(hotels.count).to eq(1)
+      expect(hotel_ids).to_not include('f8c9')
+    end
+  end
+
+  describe 'GET /v1/hotels/hotelId.json' do
+    it 'show detail of the hotel' do
+      get v1_hotel_path(format: 'json', id: 'f8c9')
+      expect(response).to have_http_status(200)
+      hotel = JSON.parse(response.body)
+      expect(hotel['id']).to eq('f8c9')
+      expect(hotel['destination_id']).to eq 1122
     end
   end
 end
